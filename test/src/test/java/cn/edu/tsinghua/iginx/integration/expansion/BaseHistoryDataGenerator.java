5f4fe92570301e321ae5f0d89ba8b60d8aa9df99
YuhuangLin <149472644@qq.com>
2025-12-15 19:44:33 +0800

    fix(test): add storageunit into path in DB-CE

--- test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/BaseHistoryDataGenerator.java
+++ test/src/test/java/cn/edu/tsinghua/iginx/integration/expansion/BaseHistoryDataGenerator.java
@@ -100,11 +100,11 @@ public abstract class BaseHistoryDataGenerator {
 
   public void writeHistoryDataToExp() {
     writeHistoryData(expPort, EXP_PATH_LIST, EXP_DATA_TYPE_LIST, EXP_VALUES_LIST);
+    writeHistoryData(expPort, EXP_PATH_LIST3, EXP_DATA_TYPE_LIST, EXP_VALUES_LIST);
   }
 
   public void writeExtendedHistoryDataToExp() {
     writeHistoryData(expPort, EXP_EXTEND_PATH_LIST, EXP_DATA_TYPE_LIST, EXP_VALUES_LIST);
-    writeHistoryData(expPort, EXP_EXTEND_PATH_LIST1, EXP_DATA_TYPE_LIST, EXP_VALUES_LIST);
     writeHistoryData(
         expPort,
         EXP_PATH_LIST,
